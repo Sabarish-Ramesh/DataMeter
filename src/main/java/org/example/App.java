30e8b2a07f144f7912d4f27d8da90fbb40a1b60a
rs-drawz <sabarishr.21it@kongu.edu>
2025-05-24 11:21:51 +0530

    Done for multiple files successfully -printing in console as expected

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,7 @@ import org.example.models.UsageSummary;
 import org.example.parser.ParseData;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -18,29 +19,38 @@ import java.util.Map;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         //single input
 //        String line = "9000600600|InAir1234|0|13456|No";
-        String filePath = "src/main/data/january.txt";
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            //creating the line
-            String line;
-            //creating map
-            HashMap<String, UsageSummary> map = new HashMap<>();
-            while((line = reader.readLine())!=null) {
-                try {
-                    //parsing the string using '|' ,it returns an object for a single input
-                    UsageRecord record = ParseData.parserData(line);
+//        String filePath = "src/main/data/january.txt";
+        File Folder = new File("src/main/data");
+        File[] files = Folder.listFiles((dir, name) -> name.endsWith(".txt"));
 
-                    //get mobileno and aggregate it using UsageSummary
-                    String mobileno = record.getMobileno();
-                    //add a new mobile number
-                    map.putIfAbsent(mobileno, new UsageSummary());
-                    //aggregate it the exist mobile number
-                    map.get(mobileno).aggregate(record);
+        if (files == null || files.length == 0) {
+            System.out.println("No data files found.");
+            return;
+        }
+        //creating map
+        HashMap<String, UsageSummary> map = new HashMap<>();
+        for (File file : files) {
+            System.out.println("Processing file: " + file.getName());
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                //creating the line
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    try {
+                        //parsing the string using '|' ,it returns an object for a single input
+                        UsageRecord record = ParseData.parserData(line);
+
+                        //get mobileno and aggregate it using UsageSummary
+                        String mobileno = record.getMobileno();
+                        //add a new mobile number
+                        map.putIfAbsent(mobileno, new UsageSummary());
+                        //aggregate it the exist mobile number
+                        map.get(mobileno).aggregate(record);
 //                    {
-                    //cost calculation -feed created object
+                        //cost calculation -feed created object
 //                    double cost = CalculateCost.Calculate(record);
 //                    System.out.printf("%s | %s | %s | %s | %s | %.0f\n",
 //                            record.getMobileno(),
@@ -51,34 +61,37 @@ public class App
 //                            cost
 //                    );
 //                }
+                    }
+                    catch (IllegalArgumentException e) {
+                        System.out.println("skip the invalid line: " + line);
+                        System.out.println("Reason: " + e.getMessage() + "\n");
+                    }
                 }
-                catch (IllegalArgumentException e){
-                    System.out.println("skip the invalid line: "+line);
-                    System.out.println("Reason: " + e.getMessage()+"\n");
-                }
+
+
+
             }
-            System.out.println("Mobile Number|4G|5G|4G Roaming|5G Roaming|Cost");
-            //iterate the map and print the data
-            for(Map.Entry<String,UsageSummary> data:map.entrySet())
-            {
-                //get mobile number
-                String mobile = data.getKey();
-                //get values for the related mobile number
-                UsageSummary summary = data.getValue();
-                double cost = CalculateCost.Calculate(summary);
-                System.out.printf("%s   | %s | %s | %s | %s | %.0f\n",
-                        mobile,
-                        summary.getHome4g(),
-                        summary.getHome5g(),
-                        summary.getRoaming4g(),
-                        summary.getRoaming5g(),
-                        cost
-                );
+            //if there is error while reading the  input throw this error
+            catch (IOException e) {
+                System.out.println(e.getMessage());
             }
         }
-        //if there is error while reading the  input throw this error
-        catch (IOException e){
-            System.out.println(e.getMessage());
+        System.out.println("Mobile Number|4G|5G|4G Roaming|5G Roaming|Cost");
+        //iterate the map and print the data
+        for (Map.Entry<String, UsageSummary> data : map.entrySet()) {
+            //get mobile number
+            String mobile = data.getKey();
+            //get values for the related mobile number
+            UsageSummary summary = data.getValue();
+            double cost = CalculateCost.Calculate(summary);
+            System.out.printf("%s   | %s | %s | %s | %s | %.0f\n",
+                    mobile,
+                    summary.getHome4g(),
+                    summary.getHome5g(),
+                    summary.getRoaming4g(),
+                    summary.getRoaming5g(),
+                    cost
+            );
         }
     }
 }
