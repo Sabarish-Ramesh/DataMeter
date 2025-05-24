4669a4b3e2e7213fd46b42d27998748365e603d6
rs-drawz <sabarishr.21it@kongu.edu>
2025-05-24 11:11:50 +0530

    Done validation for a single file-ready for multi files

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,7 @@ import java.util.Map;
 /**
  * Hello world!
  *
+ *
  */
 public class App 
 {
--- src/main/java/org/example/parser/ParseData.java
+++ src/main/java/org/example/parser/ParseData.java
@@ -9,7 +9,7 @@ public class ParseData {
 //        System.out.println(part.length);
         //validation:string must have 5 parts
         if (part.length != 5) {
-            throw new IllegalArgumentException("Invalid input line: " + line);
+            throw new IllegalArgumentException("String must contain 5 parts: " + line);
         }
 
         String mobileno = part[0].trim();
@@ -20,10 +20,31 @@ public class ParseData {
         }
         //storing the splitted value
         String towername = part[1].trim();
-        int data4G = Integer.parseInt(part[2].trim());
-        int data5G = Integer.parseInt(part[3].trim());
+        int data4G, data5G;
+        try {
+            data4G = Integer.parseInt(part[2].trim());
+            data5G = Integer.parseInt(part[3].trim());
+            //check if parsed is less than 0
+            if (data4G < 0 || data5G < 0) {
+                throw new IllegalArgumentException("Data values must be not negative");
+            }
+        }
+        //else not a number
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid 4G or 5G data: must be a number");
+        }
         //equalsIgnoreCase--it is more optimal
-        boolean isRoaming = part[4].trim().equalsIgnoreCase("yes");
+//        boolean isRoaming = part[4].trim().equalsIgnoreCase("yes");
+
+        String roaming = part[4].trim().toLowerCase();
+        boolean isRoaming;
+        if (roaming.equals("yes")) {
+            isRoaming = true;
+        } else if (roaming.equals("no")) {
+            isRoaming = false;
+        } else {
+            throw new IllegalArgumentException("Roaming value must be 'Yes' or 'No'");
+        }
         return new UsageRecord(mobileno,towername,data4G,data5G,isRoaming);
     }
 }
